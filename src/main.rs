321cf13656bca4b82c001412968b6a4c837ab7ca
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2024-12-14 15:16:03 +0000

    init main

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+    println!("Searching for {query} in {file_path} ...\n...\n...");
+
+    let file_contents: String = fs::read_to_string(file_path)
+                                .expect("Failed to read the file.");
+
+    println!("Successfully read the file:\n{file_contents}")
+
+
+
+}
