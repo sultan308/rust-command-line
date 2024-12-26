9199241c5d57b01cddfcc1260a12532afa7ac205
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2024-12-26 20:40:29 +0000

    refactor to use eprintln macro for errors to direct it to standard error stream

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep::handle;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cli_request: Request = Request::build(&args).unwrap_or_else(|err|{
-        println!("Failed to parse args: {}",err);
+        eprintln!("Failed to parse args: {}",err);
         process::exit(1)
     });
     println!("Searching for {} in {} ...\n...\n...", &cli_request.query, &cli_request.file_path);
     if let Err(e) = handle(cli_request) {
-        println!("Encountered the following error while fulfilling your request:\n{}", e);
+        eprintln!("Encountered the following error while fulfilling your request:\n{}", e);
         process::exit(1)
     }
 }
